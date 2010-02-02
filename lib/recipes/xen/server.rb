b14fdf6f737bb3368f3301788d84e75bab50c22f
Eric Marden <eric@xentek.net>
2010-02-02 01:35:14 -0600

    MOD: code formatting

--- lib/recipes/xen/server.rb
+++ lib/recipes/xen/server.rb
@@ -4,7 +4,7 @@ end
 
 namespace :xen do
   namespace :server do
-    
+
     namespace :apache do
       task :restart, :roles => :web do
         sudo "/etc/init.d/apache restart"
@@ -24,29 +24,28 @@ namespace :xen do
     end
 
     namespace :cache do
-      
       namespace :squid do
         task :restart do, :roles => :cache do
           sudo "/etc/init.d/squid restart"
         end
       end
-      
+
       namespace :memcached do
         task :restart, :roles => :cache do
           sudo "/etc/init.d/memcached restart"
         end
       end
     end
-    
+
     namespace :mysql do
       task :restart, :roles => db do
         sudo "/etc/init.d/mysql restart"
       end
-      
+
       task :backup, :roles => db do
         run "mysqldump --add-drop-database #{dbname} > #{shared_path}/sql/#{dbname}-#{release_name}.sql"
       end
-      
+
       task :restore, :roles =>db do
         run "mysql -u #{dbuser} -p < #{shared_path}/sql/#{dbname}-#{release_name}.sql"
       end
