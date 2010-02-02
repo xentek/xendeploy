bb38dc04485af8bf1d6bc7ab7ce5737b52cbe956
Eric Marden <eric@xentek.net>
2010-02-02 01:46:47 -0600

    MOD: formatting to xenify MOD: fleshed out xen:deploy module with a few tasks

--- lib/recipes/xen/deploy.rb
+++ lib/recipes/xen/deploy.rb
@@ -4,6 +4,26 @@ end
 
 namespace :xen do
   namepace :deploy do
-    
+    task :default
+      update
+      update_code
+      strategy.deploy!
+      finalize_update
+      symlink
+      restart
+    end
   end
+
+  desc "Symlinks the current release to the public directory"
+  task :makepublic, :roles => :app do
+    run "rm -f #{location}/public && ln -nfs #{deploy_to}/current #{location}/public"
+  end
+
+  namespace :wordpress do
+    desc "Symlink the uploads folder"
+    task :uploads, :roles => :app do
+      run "ln -nfs #{deploy_to}/#{shared_dir}/uploads #{deploy_to}/current/wp-content/uploads"
+    end
+  end
+
 end
