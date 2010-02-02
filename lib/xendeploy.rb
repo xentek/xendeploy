84954ab754b8b4c4661c4de6f97bae0e2b943105
Eric Marden <eric@xentek.net>
2010-02-02 17:57:42 -0600

    MOD: formatting mostly

--- lib/xendeploy.rb
+++ lib/xendeploy.rb
@@ -2,4 +2,4 @@ unless Capistrano::Configuration.respond_to?(:instance)
   abort "xen:deploy requires Capistrano 2"
 end
 
-require "#{File.dirname(__FILE__)}/recipes"
+require "#{File.dirname(__FILE__)}/recipes/xen"
