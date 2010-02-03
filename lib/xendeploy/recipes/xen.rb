97607cd85fbcaddf0f3e56827bcaae13aa546e9b
Eric Marden <eric@xentek.net>
2010-02-02 23:25:22 -0600

    DEL: dropped the xen.rb file as it was redundant

--- lib/xendeploy/recipes/xen.rb
+++
@@ -1,7 +0,0 @@
-unless Capistrano::Configuration.respond_to?(:instance)
-  abort "xendeploy requires Capistrano 2"
-end
-
-require "#{File.dirname(__FILE__)}/xen/deploy"
-require "#{File.dirname(__FILE__)}/xen/server"
-require "#{File.dirname(__FILE__)}/xen/build"
